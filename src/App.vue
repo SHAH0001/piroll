aedad2bf39ed0ad74c8ed8025a1b42d1fb8dc0ab
SHAH0001 <vlad-t-97@mail.ru>
2020-10-13 19:59:18 +0600

    features done

--- src/App.vue
+++ src/App.vue
@@ -69,6 +69,129 @@
       </div>
       <div class="professional-skills__right"></div>
     </div>
+    <div class="about-our-work">
+      <div class="container">
+        <div class="about-our-work__items">
+          <div class="about-our-work__item portfolio">
+            <h4 class="about-our-work__conut">548</h4>
+            <p class="about-our-work__text">Projects completed</p>
+          </div>
+          <div class="about-our-work__item clock">
+            <h4 class="about-our-work__conut">1465</h4>
+            <p class="about-our-work__text">Working hours</p>
+          </div>
+          <div class="about-our-work__item star">
+            <h4 class="about-our-work__conut">612</h4>
+            <p class="about-our-work__text">Positive feedbacks</p>
+          </div>
+          <div class="about-our-work__item like">
+            <h4 class="about-our-work__conut">735</h4>
+            <p class="about-our-work__text">Happy clients</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="gallery">
+      <a 
+        v-for="(item, index) in 8" 
+        :key="index"
+        href="#" 
+        class="gallery__item"
+      >
+        <img  
+          class="gallery__img" 
+          :src="getImage('img_' + item + '.jpg')" 
+          alt=""
+        />
+        <div class="gallery__overlay"></div>
+      </a>
+    </div>
+    <h3 class="load-more">Load more works</h3>
+    <div class="our-work">
+      <div class="container">
+        <h3 class="our-work__title">Our Work Process</h3>
+        <p class="our-work__description">
+          Was years it seasons was there form he in in them together over that, 
+          third sixth gathered female creeping bearing behold years.
+        </p>
+        <div class="our-work__video-wrapper">
+          <video 
+            id="mainVideo"
+            class="our-work__video" 
+            poster="./assets/images/video-poster.jpg" src="./assets/videos/video.mp4"
+          ></video>
+          <button 
+            @click="startStop()" 
+            :class="{'video-play' : videoPlay}"
+            class="our-work__play-stop" 
+            type="button"
+          >
+            <span :class="{'video-play' : videoPlay}" class="play-stop"></span>
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="features">
+      <div class="container">
+        <div class="features__wrapper">
+          <div class="features__item diamond">
+            <a href="#" class="features__title">ui / ux design</a>
+            <p class="features__description">
+              Be set fourth land god darkness 
+              make it wherein own
+            </p>
+          </div>
+          <div class="features__item arc">
+            <a href="#" class="features__title">web development</a>
+            <p class="features__description">
+              A she'd them bring void moving 
+              third she'd kind fill
+            </p>
+          </div>
+          <div class="features__item phone">
+            <a href="#" class="features__title">app / mobile</a>
+            <p class="features__description">
+              Dominion man second spirit he,
+              earth they're creeping
+            </p>
+          </div>
+          <div class="features__item joy">
+            <a href="#" class="features__title">game design</a>
+            <p class="features__description">
+              Morning his saying moveth it 
+              multiply appear life be</p>
+          </div>
+          <div class="features__item plane">
+            <a href="#" class="features__title">SEO / marketing</a>
+            <p class="features__description">
+              Give won't after land fill creeping 
+              meat you, may
+            </p>
+          </div>
+          <div class="features__item star">
+            <a href="#" class="features__title">photography</a>
+            <p class="features__description">
+              Creepeth one seas cattle grass 
+              give moving saw give
+            </p>
+          </div>
+          <div class="features__item magic-wand">
+            <a href="#" class="features__title">graphic design</a>
+            <p class="features__description">
+              Open, great whales air rule for, 
+              fourth life whales
+            </p>
+          </div>
+          <div class="features__item paint-bucket">
+            <a href="#" class="features__title">illustrations</a>
+            <p class="features__description">
+              Whales likeness hath, man kind 
+              for them air two won't
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
     <footer style="height:250px;"></footer>
   </div>
 </template>
@@ -87,12 +210,25 @@ export default {
         'testimonials',
         'contact'
       ],
-      showMobileMenu: false
+      showMobileMenu: false,
+      videoPlay: false
     }
   },
   methods: {
     getImage(path) {
       return require('./assets/images/' + path);
+    },
+    startStop() {
+      let video = document.getElementById('mainVideo')
+
+      if(video.paused) {
+        video.play()
+        this.videoPlay = true
+      }
+      else {
+        video.pause()
+        this.videoPlay = false
+      }
     }
   }
 
@@ -441,6 +577,316 @@ export default {
       background-color: $mainColor;
     }
 
+  }
+
+  .portfolio {
+    background-image: url(./assets/images/portfolio.png);
+  }
+
+  .clock {
+    background-image: url(./assets/images/clock.png);
+  }
+
+  .star {
+    background-image: url(./assets/images/star.png);
+  }
+
+  .like {
+    background-image: url(./assets/images/like.png);    
+  }
+
+  .about-our-work {
+    
+    background-color: $mainColor;
+
+    &__items {
+      display: flex;
+      justify-content: space-between;
+      padding: 95px 0 99px 0;
+      margin: 0 0 0 -14px;
+
+      @include max-width-1260 {
+        margin: 0;
+      }
+
+      @include max-width-1190 {
+        justify-content: space-around;
+        flex-wrap: wrap;
+        padding: 50px 0;
+      }
+    }
+
+    &__item {
+      width: 22%;
+      background-position: 0 7px;
+      background-repeat: no-repeat;
+      color: #fff;
+      
+      @include max-width-1190 {
+        width: 45%;
+        text-align: center;
+        background-position: 25% 7px;
+        margin: 20px 0;
+      }
+
+      @include max-width-750 {
+        background-position: 50% 0;
+        padding: 60px 0 0 0;
+      }
+
+      @include max-width-550 {
+        width: 100%;
+      }
+    }
+
+    &__conut {
+      font-size: 18px;
+      margin: 0 0 0 66px;
+
+      @include max-width-750 {
+        margin: 0 0 10px 0;
+      }
+    }
+
+    &__text {
+      font-size: 12px;
+      margin: 9px 0 0 67px;
+      text-transform: uppercase;
+
+      @include max-width-750 {
+        margin: 0 0 10px 0;
+      }
+    }
+
+  }
+
+  .gallery {
+    display: flex;
+    flex-wrap: wrap;
+
+    &__item {
+      position: relative;
+      width: 25%;
+
+      @include max-width-850 {
+        width: 33.3333%;
+      }
+
+      @include max-width-650 {
+        width: 50%;
+      }
+
+      @include max-width-450 {
+        width: 100%;
+      }
+    }
+
+    &__img {
+      display: block;
+      width: 100%;
+      height: 100%;
+    }
+
+    &__overlay {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: transparent;
+      transition: all .3s;
+      background-position: 50%;
+      background-repeat: no-repeat;
+    }
+
+  }
+
+  .gallery__overlay:hover {
+    background-color: rgba(12, 12, 12, 0.5);
+    background-image: url(./assets/images/look.png);
+  }
+
+  .load-more {
+    font-size: 12px;
+    color: #8e8e8e;
+    cursor: pointer;
+    margin: 0;
+    text-transform: uppercase;
+    background-color: #f5f5f5;
+    text-align: center;
+    padding: 42px 0;
+  }
+
+  .our-work {
+    text-align: center;
+    margin: 94px 0 0 0;
+
+    &__title {
+      font-size: 30px;
+      color: $mainTextColor;
+      margin: 0 0 27px 0;
+    }
+
+    &__description {
+      font-size: 16px;
+      color: $textColor;
+      margin: 0 287px;
+      line-height: 24px;
+
+      @include max-width-950 {
+        margin: 0;
+      }
+    }
+
+    &__video-wrapper {
+      position: relative;
+    }
+
+    &__video {
+      display: block;
+      width: 100%;
+      max-width: 1170px;
+      height: 570px;
+      margin: 53px 0 0 0;
+
+      @include max-width-950 {
+        margin: 0;
+        height: auto;
+      }
+    }
+
+    &__play-stop {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 85px;
+      height: 85px;
+      background-color: $mainColor;
+      border-radius: 50%;
+      border: none;
+      outline: none;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      margin: auto;
+      cursor: pointer;
+    }
+
+  }
+
+  .our-work__play-stop:hover {
+    background-color: $mainColor;
+  }
+
+  .our-work__play-stop:hover .play-stop {
+    border-color: transparent transparent transparent #ffffff;
+  }
+
+  .play-stop {
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-width: 8.5px 0 8.5px 11px;
+    border-color: transparent transparent transparent #ffffff;
+  }
+
+  .video-play {
+    background-color: transparent;
+    border-color: transparent transparent transparent transparent;
+  }
+
+  .features {
+    background-color: #f5f5f5;
+    margin: 101px 0 0 0;
+
+    @include max-width-950 {
+      margin: 0;
+    }
+
+    &__wrapper {
+      display: flex;
+      flex-wrap: wrap;
+      padding: 101px 0 40px 0;
+
+      @include max-width-950 {
+        justify-content: center;
+      }
+    }
+
+    &__item {
+      width: 25%;
+      background-repeat: no-repeat;
+      background-position: 50% 0;
+      text-align: center;
+      margin: 0 0 56px 0;
+      padding: 0 10px;
+
+      @include max-width-950 {
+        width: 33.3333%;  
+      }
+
+      @include max-width-650 {
+        width: 50%;  
+      }
+
+      @include max-width-550 {
+        width: 100%;  
+      }
+    }
+
+    &__title {
+      display: inline-block;
+      font-size: 12px;
+      font-weight: bold;
+      color: $mainTextColor;
+      margin: 67px 0 0 0;
+      text-transform: uppercase;
+      text-decoration: none;
+    }
+
+    &__description {
+      font-family: $notoSans;
+      font-size: 16px;
+      color: $textColor;
+      margin: 11px 0 0 0;
+      line-height: 24px;
+      padding: 0 1px;
+    }
+
+  }
+
+  .diamond {
+    background-image: url(./assets/images/diamond.png);
+  }
+
+  .arc {
+    background-image: url(./assets/images/arc.png);
+  }
+
+  .phone {
+    background-image: url(./assets/images/phone.png);
+  }
+
+  .joy {
+    background-image: url(./assets/images/joy.png);
+  }
+
+  .plane {
+    background-image: url(./assets/images/plane.png);
+  }
+
+  .star {
+    background-image: url(./assets/images/star_2.png);
+  }
+
+  .magic-wand {
+    background-image: url(./assets/images/magic_wand.png);
+  }
+
+  .paint-bucket {
+    background-image: url(./assets/images/paint_bucket.png);
   }
 
 </style>
